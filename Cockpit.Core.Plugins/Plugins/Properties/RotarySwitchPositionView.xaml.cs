21a614642c0757946ad547e5d55bfca97bd78e11
Frenchy62620 <tkiel@free.fr>
2019-08-13 12:22:14 +0200

    a

---
+++ Cockpit.Core.Plugins/Plugins/Properties/RotarySwitchPositionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Cockpit.Core.Plugins.Plugins.Properties
+{
+    /// <summary>
+    /// Logique d'interaction pour RotarySwitchPosition.xaml
+    /// </summary>
+    public partial class RotarySwitchPositionView : UserControl
+    {
+        public RotarySwitchPositionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Cockpit.Core.Plugins/Plugins/Properties/RotarySwitchPositionsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System.Collections.ObjectModel;
+
+namespace Cockpit.Core.Plugins.Plugins.Properties
+{
+    public class RotarySwitchPositionsViewModel:PropertyChangedBase
+    {
+        public RotarySwitchPositionsViewModel()
+        {
+        }
+
+        public ObservableCollection<RotarySwitchPositionsViewModel> RotarySwitchPositions { get; private set; }
+
+        private RotarySwitchPositionsViewModel selected;
+        public RotarySwitchPositionsViewModel SelectedRotarySwitchPosition
+        {
+            get { return selected; }
+            set
+            {
+                if (selected != value)
+                {
+                    selected = value;
+                    NotifyOfPropertyChange();
+                }
+            }
+
+        }
+}
